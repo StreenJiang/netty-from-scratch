b170f9e9b9e7daf3ea52cd3b1975a13036653600
Streen Jiang <streen.jiang@outlook.com>
2026-03-19 16:49:16 +0800

    Add ByteBuf, EmbeddedChannel and Pipeline tests

---
+++ src/test/java/com/netty/netty/TestPipeline.java
@@ -0,0 +1,81 @@
+package com.netty.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.ChannelPromise;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+public class TestPipeline {
+    public static void main(String[] args) {
+        new ServerBootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        // 1. 通过 channel 拿到 pipeline
+                        ChannelPipeline pipeline = ch.pipeline();
+
+                        // 2. 添加处理器 head -> h1 -> tail
+                        //   addLast 是将新的 handler 添加到 head 和 tail 之间
+                        pipeline.addLast("h1", new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                log.debug("h1");
+                                super.channelRead(ctx, msg);
+                            }
+                        });
+                        pipeline.addLast("h2", new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                log.debug("h2");
+                                super.channelRead(ctx, msg); // 将数据传递给下一个 handler，否则调用链就断掉了
+                            }
+                        });
+                        pipeline.addLast("h3", new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                log.debug("h3");
+                            }
+                        });
+                        pipeline.addLast("h4", new ChannelOutboundHandlerAdapter() {
+                            @Override
+                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise)
+                                    throws Exception {
+                                log.debug("h4");
+                                super.write(ctx, msg, promise);
+                            }
+                        });
+                        pipeline.addLast("h5", new ChannelOutboundHandlerAdapter() {
+                            @Override
+                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise)
+                                    throws Exception {
+                                log.debug("h5");
+                                super.write(ctx, msg, promise);
+                            }
+                        });
+                        pipeline.addLast("h6", new ChannelOutboundHandlerAdapter() {
+                            @Override
+                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise)
+                                    throws Exception {
+                                log.debug("h6");
+                                super.write(ctx, msg, promise);
+                            }
+                        });
+                    }
+                })
+                .bind(8080);
+    }
+}
